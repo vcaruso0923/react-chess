8335cc7d9ac369e362f8bb6b9b7d20eb310fa284
Vincent Caruso <vincentcaruso97.com>
2022-05-11 13:49:07 -0700

    horse (knight) logic added)

--- src/utils/piecesLogic/horseLogic.js
+++ src/utils/piecesLogic/horseLogic.js
@@ -10,6 +10,44 @@ import {
 export const horseMoveEval = (firstClick, secondClick, piecesLocation) => {
     if (piecesLocation[firstClick].includes('horse')) {
         // Must move in L shape
+        // up 2 right 1
+        if (
+            // up 2 right 1
+            secondClick !==
+                String.fromCharCode(pieceToMoveCharacterCode(firstClick) + 1) +
+                    (pieceToMoveNumber(firstClick) + 2).toString() &&
+            // up 2, left 1
+            secondClick !==
+                String.fromCharCode(pieceToMoveCharacterCode(firstClick) - 1) +
+                    (pieceToMoveNumber(firstClick) + 2).toString() &&
+            // up 1, right 2
+            secondClick !==
+                String.fromCharCode(pieceToMoveCharacterCode(firstClick) + 2) +
+                    (pieceToMoveNumber(firstClick) + 1).toString() &&
+            // up 1, left 2
+            secondClick !==
+                String.fromCharCode(pieceToMoveCharacterCode(firstClick) - 2) +
+                    (pieceToMoveNumber(firstClick) + 1).toString() &&
+            // down 2, left 1
+            secondClick !==
+                String.fromCharCode(pieceToMoveCharacterCode(firstClick) - 1) +
+                    (pieceToMoveNumber(firstClick) - 2).toString() &&
+            // down 2, right 1
+            secondClick !==
+                String.fromCharCode(pieceToMoveCharacterCode(firstClick) + 1) +
+                    (pieceToMoveNumber(firstClick) - 2).toString() &&
+            // down 1, right 2
+            secondClick !==
+                String.fromCharCode(pieceToMoveCharacterCode(firstClick) + 2) +
+                    (pieceToMoveNumber(firstClick) - 1).toString() &&
+            // down 1, left 2
+            secondClick !==
+                String.fromCharCode(pieceToMoveCharacterCode(firstClick) - 2) +
+                    (pieceToMoveNumber(firstClick) - 1).toString()
+        ) {
+            return false;
+        }
+
         // All evaluated
         return true;
     } else {
