b950451bf41e4271f358443931f8c7fe6d537160
Vincent Caruso <vincentcaruso97.com>
2022-05-11 14:08:14 -0700

    queen logic

--- src/utils/piecesLogic/queenLogic.js
+++ src/utils/piecesLogic/queenLogic.js
@@ -10,8 +10,137 @@ import {
 export const queenMoveEval = (firstClick, secondClick, piecesLocation) => {
     if (piecesLocation[firstClick].includes('queen')) {
         // Can't move unless diagonal
-        // Can't move unless horizontal/vertical
-        // Can't move unless horsin' around
+        if (
+            Math.abs(
+                pieceToMoveNumber(firstClick) - destinationNumber(secondClick)
+            ) !==
+            Math.abs(
+                pieceToMoveCharacterCode(firstClick) -
+                    destinationCharacterCode(secondClick)
+            )
+        ) {
+            // Can't move unless horizontal/vertical
+            if (
+                pieceToMoveNumber(firstClick) !==
+                    destinationNumber(secondClick) &&
+                destinationCharacterLetter(secondClick) !==
+                    pieceToMoveCharacterLetter(firstClick)
+            ) {
+                return false;
+            }
+        }
+
+        // Can't move over pieces
+        // Diagonals
+        // up & right
+        if (
+            pieceToMoveNumber(firstClick) < destinationNumber(secondClick) &&
+            pieceToMoveCharacterCode(firstClick) <
+                destinationCharacterCode(secondClick)
+        ) {
+            // prettier-ignore
+            for (let i = 1 ; i < Math.abs((pieceToMoveNumber(firstClick) - destinationNumber(secondClick))) ; i++) {
+                let newCode = String.fromCharCode(pieceToMoveCharacterCode(firstClick) + i) + (pieceToMoveNumber(firstClick) + i).toString()
+                if (piecesLocation[newCode].includes('white') || piecesLocation[newCode].includes('black')) {
+                    return false
+                }
+            }
+        }
+        // up & left
+        if (
+            pieceToMoveNumber(firstClick) < destinationNumber(secondClick) &&
+            pieceToMoveCharacterCode(firstClick) >
+                destinationCharacterCode(secondClick)
+        ) {
+            // prettier-ignore
+            for (let i = 1 ; i < Math.abs((pieceToMoveNumber(firstClick) - destinationNumber(secondClick))) ; i++) {
+                let newCode = String.fromCharCode(pieceToMoveCharacterCode(firstClick) - i) + (pieceToMoveNumber(firstClick) + i).toString()
+                if (piecesLocation[newCode].includes('white') || piecesLocation[newCode].includes('black')) {
+                    return false
+                }
+            }
+        }
+        // down & left
+        if (
+            pieceToMoveNumber(firstClick) > destinationNumber(secondClick) &&
+            pieceToMoveCharacterCode(firstClick) >
+                destinationCharacterCode(secondClick)
+        ) {
+            // prettier-ignore
+            for (let i = 1 ; i < Math.abs((pieceToMoveNumber(firstClick) - destinationNumber(secondClick))) ; i++) {
+                let newCode = String.fromCharCode(pieceToMoveCharacterCode(firstClick) - i) + (pieceToMoveNumber(firstClick) - i).toString()
+                if (piecesLocation[newCode].includes('white') || piecesLocation[newCode].includes('black')) {
+                    return false
+                }
+            }
+        }
+        // down & right
+        if (
+            pieceToMoveNumber(firstClick) > destinationNumber(secondClick) &&
+            pieceToMoveCharacterCode(firstClick) <
+                destinationCharacterCode(secondClick)
+        ) {
+            // prettier-ignore
+            for (let i = 1 ; i < Math.abs((pieceToMoveNumber(firstClick) - destinationNumber(secondClick))) ; i++) {
+                let newCode = String.fromCharCode(pieceToMoveCharacterCode(firstClick) + i) + (pieceToMoveNumber(firstClick) - i).toString()
+                if (piecesLocation[newCode].includes('white') || piecesLocation[newCode].includes('black')) {
+                    return false
+                }
+            }
+        }
+
+        // Horizontal / Vertical
+        // Can't move over pieces horizontally
+        if (pieceToMoveNumber(firstClick) === destinationNumber(secondClick)) {
+            // moving left
+            // prettier-ignore
+            if (destinationCharacterCode(secondClick) < pieceToMoveCharacterCode(firstClick)) {
+                for (let i = (destinationCharacterCode(secondClick) + 1) ; i < pieceToMoveCharacterCode(firstClick) ; i++) {
+                    let newCode = String.fromCharCode(i) + firstClick.slice(-1)
+                    if (piecesLocation[newCode].includes('white') || piecesLocation[newCode].includes('black')) {
+                        return false
+                    }
+                }
+            }
+            // moving right
+            // prettier-ignore
+            if (pieceToMoveCharacterCode(firstClick) < destinationCharacterCode(secondClick)) {
+                for (let i = (pieceToMoveCharacterCode(firstClick) + 1) ; i < destinationCharacterCode(secondClick) ; i++) {
+                    let newCode = String.fromCharCode(i) + firstClick.slice(-1)
+                    if (piecesLocation[newCode].includes('white') || piecesLocation[newCode].includes('black')) {
+                        return false
+                    }
+                }
+            }
+        }
+
+        // Can't move over pieces vertically
+        if (
+            pieceToMoveCharacterCode(firstClick) ===
+            destinationCharacterCode(secondClick)
+        ) {
+            console.log('vert');
+            // moving down
+            // prettier-ignore
+            if (destinationNumber(secondClick) < pieceToMoveNumber(firstClick)) {
+                for (let i = (destinationNumber(secondClick) + 1) ; i < pieceToMoveNumber(firstClick) ; i++) {
+                    let newCode = pieceToMoveCharacterLetter(firstClick) + i.toString()
+                    if (piecesLocation[newCode].includes('white') || piecesLocation[newCode].includes('black')) {
+                        return false
+                    }
+                }
+            }
+            // moving up
+            // prettier-ignore
+            if (pieceToMoveNumber(firstClick) < destinationNumber(secondClick)) {
+                for (let i = (pieceToMoveNumber(firstClick) + 1) ; i < destinationNumber(secondClick) ; i++) {
+                    let newCode = pieceToMoveCharacterLetter(firstClick) + i.toString()
+                    if (piecesLocation[newCode].includes('white') || piecesLocation[newCode].includes('black')) {
+                        return false
+                    }
+                }
+            }
+        }
         // All evaluated
         return true;
     } else {
