41156849579b0875a6e8c2c1f8c06ed30b08c093
Vincent Caruso <vincentcaruso97.com>
2022-06-07 19:06:24 -0700

    some styling

--- client/src/Gameboard/index.js
+++ client/src/Gameboard/index.js
@@ -88,6 +88,7 @@ function Gameboard() {
     }, [piecesLocation]);
 
     const boardClickHandler = (e) => {
+        roomIsFullNotif();
         e.preventDefault();
         // variables to send to socket (can't wait for state update)
         let piecesLocationToSend;
